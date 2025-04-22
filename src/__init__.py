c760edd3a9ef9e2f19efa85c8032eaca41fbf354
Fabio Coimbra <fabiogc1989@gmail.com>
2025-04-22 22:58:24 +0100

    Fix import in init python file of src package

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
-from configs import AppConfig
+
 from flask import Flask
+from src.configs import AppConfig
 from src.extensions import db
 from src.shop_service.views import views as shop_service_views
 
