d0dc245753eda4806230923e6b424e00f795e1f7
Fabio Coimbra <fabiogc1989@gmail.com>
2025-05-09 23:24:53 +0100

    remove unsed imports

--- init_db.py
+++ init_db.py
@@ -3,8 +3,6 @@ from src.extensions import db, engine
 import src.shop_service.models
 import src.user.models
 import src.person.models
-import src.person.customer.models
-import src.person.employee.models
 
 
 if __name__ == '__main__':
