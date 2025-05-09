f624a0adb8325173feb094f51ecbc57a5b5221b7
Fabio Coimbra <fabiogc1989@gmail.com>
2025-05-09 23:27:43 +0100

    clean user model

--- src/user/models.py
+++ src/user/models.py
@@ -2,11 +2,8 @@ from src.extensions import Base, db
 from sqlalchemy import String
 from sqlalchemy.orm import mapped_column, relationship
 
-class User(Base, db.Model):
+class User(Base):
     __tablename__ = 'user'
 
     username = mapped_column(String(256), nullable=False, unique=True)
     password = mapped_column(String(256), nullable=False)
-
-    # Relationships
-    person = relationship('Person', back_populates='user')
